12a704e7bf7892e7c008752b53c933b195cb8417
jose-ant-1 <jgonsol1510@g.educaand.es>
2025-10-22 22:03:11 +0200

    implementacion de typeOf

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -1,6 +1,6 @@
 console.log('Happy developing ✨')
 
-const personas = [{
+const person = [{
     "nombre":"Noon",
     "edad":6,
     "aficiones":["Deporte","Lectura","Viajar", "juegos"],
@@ -13,24 +13,36 @@ const personas = [{
     "emancipado":false
 }]
 
+const tiposVariable = {
+    "string": "text",
+    "number": "number",
+    "boolean": "checkbox"
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const mostrar = document.getElementById('mostrar');
-    personas.forEach((person) => {
+    person.forEach((person) => {
         generarFormulario(person)
     })
+
+    function comprobarTipo(formatoAComprobar) {
+        return tiposVariable[formatoAComprobar] || "text";
+
+    }
 
     function generarFormulario(objeto) {
         mostrar.innerHTML += `    <form>
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" value="${objeto.nombre}"> <br>
+        <input type="${comprobarTipo(typeof objeto.nombre)}" id="nombre" value="${Object.values(objeto)[0]}"> <br>
 
         <label for="edad">Edad:</label>
-        <input type="number" id="edad" value="${objeto.edad}"> <br>
+        <input type="${comprobarTipo(typeof objeto.edad)}" id="edad" value="${Object.values(objeto)[1]}"> <br>
 
         <label for="aficiones">Aficiones:</label>
         ${mostrarAficiones(objeto.aficiones)}<br>
         <label for="emancipado">Emancipado</label>
-        <input id="emancipado" type="checkbox" ${objeto.emancipado ? "checked" : ""}><br>
+        <input id="emancipado" type="${comprobarTipo(typeof objeto.emancipado)}" ${Object.values(objeto)[3] ? "checked" : ""}><br>
         <button type="submit">Enviar</button>
     </form> </br>`
     }
